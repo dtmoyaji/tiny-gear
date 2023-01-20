a865ef3657fce6ef217a0b9ec623e89d0735f67d
dtmoyaji <daianji@gmail.com>
2023-01-20 17:02:58 +0900

    cssでアイコンを表示する仕組みを作成。

--- src/main/java/org/tiny/gear/GearApplication.java
+++ src/main/java/org/tiny/gear/GearApplication.java
@@ -1,6 +1,9 @@
 package org.tiny.gear;
 
+import java.io.File;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.resource.SharedResourceReference;
+import org.apache.wicket.resource.FileSystemResource;
 import org.tiny.wicket.SamlWicketApplication;
 
 /**
@@ -23,7 +26,14 @@ public class GearApplication extends SamlWicketApplication {
     @Override
     public void init() {
         super.init();
+        this.mountResources();
+    }
 
+    public void mountResources() {
+        String classesPath = this.getServletContext().getRealPath("/");
+        // Webアプリの CLASSPATH の下、img の下に画像ファイルが存在する場合、、 
+        this.getSharedResources().add("img/kkrn_icon_menu_11.png", new FileSystemResource(new File(classesPath + "img/kkrn_icon_menu_11.png").toPath()));
+        this.mountResource("img/kkrn_icon_menu_11.png", new SharedResourceReference("img/kkrn_icon_menu_11.png"));
     }
 
     @Override
