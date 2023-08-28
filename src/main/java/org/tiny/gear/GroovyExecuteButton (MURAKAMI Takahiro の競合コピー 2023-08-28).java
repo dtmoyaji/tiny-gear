31b127d63d6e2dc8d1c4de5a4da9d6da81021044
楽曲作りまくりおじさん <60741124+dtmoyaji@users.noreply.github.com>
2023-08-28 21:53:56 +0900

    deleted:    "src/main/java/org/tiny/gear/ExcelFileGenerator (MURAKAMI Takahiro \343\201\256\347\253\266\345\220\210\343\202\263\343\203\224\343\203\274 2023-08-28).java" 	deleted:    "src/main/java/org/tiny/gear/GroovyExecuteButton (MURAKAMI Takahiro \343\201\256\347\253\266\345\220\210\343\202\263\343\203\224\343\203\274 2023-08-28).java" 	deleted:    "src/main/java/org/tiny/gear/panels/TableExportController (MURAKAMI Takahiro \343\201\256\347\253\266\345\220\210\343\202\263\343\203\224\343\203\274 2023-08-28).html" 	deleted:    "src/main/java/org/tiny/gear/panels/TableExportController (MURAKAMI Takahiro \343\201\256\347\253\266\345\220\210\343\202\263\343\203\224\343\203\274 2023-08-28).java" 	deleted:    "src/main/webapp/files/README (MURAKAMI Takahiro \343\201\256\347\253\266\345\220\210\343\202\263\343\203\224\343\203\274 2023-08-28).md" 	deleted:    "src/main/webapp/popuppanel (MURAKAMI Takahiro \343\201\256\347\253\266\345\220\210\343\202\263\343\203\224\343\203\274 2023-08-28).css"

---
+++ src/main/java/org/tiny/gear/GroovyExecuteButton (MURAKAMI Takahiro の競合コピー 2023-08-28).java
@@ -0,0 +1,59 @@
+package org.tiny.gear;
+
+import java.util.ArrayList;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+/**
+ *
+ * @author dtmoyaji
+ */
+public class GroovyExecuteButton extends AjaxButton {
+
+    private GearApplication application;
+
+    private Panel panel;
+    private String panelName;
+
+    private GroovyExecutor groovyExecutor;
+
+    public GroovyExecuteButton(String id, IModel<String> model, GearApplication app, Panel panel) {
+        super(id, model);
+        this.application = app;
+        this.panel = panel;
+        this.panelName = panel.getClass().getName();
+        this.groovyExecutor = new GroovyExecutor(this.application);
+    }
+
+    public GroovyExecuteButton(String id, IModel<String> model, GearApplication app, Panel panel, String panelName) {
+        super(id, model);
+        this.application = app;
+        this.panel = panel;
+        this.panelName = panelName;
+        this.groovyExecutor = new GroovyExecutor(this.application);
+    }
+
+    @Override
+    public void onSubmit(AjaxRequestTarget target) {
+        boolean result = this.groovyExecutor.execute(
+                this.getClassRelativePath(),
+                this.panel,
+                this.panelName
+        );
+        if (result) {
+            this.afterGroovyExecute(target);
+            ArrayList<Component> updateComponents = this.groovyExecutor.getUpdateComponents();
+            for (Component cmp : updateComponents) {
+                target.add(cmp);
+            }
+        }
+    }
+    
+    public void afterGroovyExecute(AjaxRequestTarget target){
+        
+    }
+
+}
